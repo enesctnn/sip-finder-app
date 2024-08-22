7202df71074992142d71ded4f25a917c15796f96
enesctnn <enespcetin@gmail.com>
2024-08-22 18:50:33 +0300

    feat: add react suspense on searching cocktails to show feedback on searching new cocktail.

--- app/(protected)/cocktails/@cocktails_list/[[...filter]]/page.tsx
+++ app/(protected)/cocktails/@cocktails_list/[[...filter]]/page.tsx
@@ -1,9 +1,25 @@
+import { CocktailStatus } from '@/components/cocktails/cocktails-list/cocktail-status/CocktailStatus';
 import { CocktailsList } from '@/components/cocktails/cocktails-list/CocktailsList';
+import { Suspense } from 'react';
+import styles from './page.module.scss';
 
 export default function CocktailsListPage({
   params: { filter },
 }: {
   params: { filter?: string };
 }) {
-  return <CocktailsList filter={filter || ''} />;
+  return (
+    <Suspense
+      fallback={
+        <CocktailStatus
+          title="Cocktails Loading . . ."
+          src="/assets/pouring-cocktail.gif"
+          alt="pouring wiskey gif."
+          className={styles.pulse}
+        />
+      }
+    >
+      <CocktailsList filter={filter || ''} />
+    </Suspense>
+  );
 }
