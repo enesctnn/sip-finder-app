1cfd4ca979d79feff9300303cc145643ef53c343
enesctnn <enespcetin@gmail.com>
2024-08-22 17:50:17 +0300

    fix: fix home redirect button

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
 import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
 } from '@/components/ui/card/Card';
-import { LinkButton } from '@/components/ui/link-button/link-button';
-import { fetchUserServerSide } from '@/utils/fetchUserServerSide';
+import { WelcomeButton } from '@/components/welcome-button/WelcomeButton';
 import Image from 'next/image';
 import styles from './page.module.scss';
 
-export default async function Home() {
-  const user = await fetchUserServerSide();
+export default function Home() {
   return (
     <div className={styles.container}>
       <Card className={styles.card}>
@@ -31,15 +29,7 @@ export default async function Home() {
           Log in to get started on your flavor adventure!
         </CardContent>
         <CardFooter>
-          <div className={styles['home-menu']}>
-            {user && <p>Welcome back {user.username}</p>}
-            <LinkButton
-              href={`${user ? '/cocktails' : '/auth/login'}`}
-              variant="link"
-            >
-              {!user ? 'Login' : `Go to cocktails`}
-            </LinkButton>
-          </div>
+          <WelcomeButton />
         </CardFooter>
       </Card>
     </div>
--- components/welcome-button/WelcomeButton.tsx
+++ components/welcome-button/WelcomeButton.tsx
@@ -1,9 +1,11 @@
-import { fetchUserServerSide } from '@/utils/fetchUserServerSide';
+'use client';
+
+import { useUserContext } from '@/store/user-context/UserContextProvider';
 import { LinkButton } from '../ui/link-button/link-button';
 import styles from './WelcomeButton.module.scss';
 
-export async function WelcomeButton() {
-  const user = await fetchUserServerSide();
+export function WelcomeButton() {
+  const user = useUserContext();
   return (
     <div className={styles['home-menu']}>
       {user && <p>Welcome back {user.username}</p>}
